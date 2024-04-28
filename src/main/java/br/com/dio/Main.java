ef0cda08f381fad2b768067a3308b4d19bfcfa9c
Junior <junior.jr.jl@gmail.com>
2024-04-28 20:32:17 -0300

    14.3

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -1,13 +1,17 @@
 package br.com.dio;
 
 import br.com.dio.persistence.EmployeeDAO;
+import org.flywaydb.core.Flyway;
 
 public class Main {
 
     private final static EmployeeDAO employeeDAO = new EmployeeDAO();
 
     public static void main(String[] args) {
-
+        var flyway = Flyway.configure()
+                .dataSource("jdbc:mysql://localhost/jdbc-sample", "root", "root")
+                .load();
+        flyway.migrate();
     }
 
 }
