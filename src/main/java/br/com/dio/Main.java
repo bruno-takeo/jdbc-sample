7d8237e84baa2d0b652159f9bcb893cb5baab591
Junior <junior.jr.jl@gmail.com>
2024-04-28 20:56:58 -0300

    14.4

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -1,7 +1,11 @@
 package br.com.dio;
 
 import br.com.dio.persistence.EmployeeDAO;
+import br.com.dio.persistence.entity.EmployeeEntity;
 import org.flywaydb.core.Flyway;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 public class Main {
 
@@ -12,6 +16,14 @@ public class Main {
                 .dataSource("jdbc:mysql://localhost/jdbc-sample", "root", "root")
                 .load();
         flyway.migrate();
+
+        /*var employee = new EmployeeEntity();
+        employee.setName("Miguel");
+        employee.setSalary(new BigDecimal("2800"));
+        employee.setBirthday(OffsetDateTime.now().minusYears(18));
+        System.out.println(employee);
+        employeeDAO.insert(employee);
+        System.out.println(employee);*/
     }
 
 }
