7d948862f73fd5f5a6625ccd4c5061ef537d83eb
Junior <junior.jr.jl@gmail.com>
2024-04-28 20:11:44 -0300

    14.2

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -1,6 +1,13 @@
 package br.com.dio;
 
+import br.com.dio.persistence.EmployeeDAO;
+
 public class Main {
 
+    private final static EmployeeDAO employeeDAO = new EmployeeDAO();
+
+    public static void main(String[] args) {
+
+    }
 
 }
---
+++ src/main/java/br/com/dio/persistence/EmployeeDAO.java
@@ -0,0 +1,29 @@
+package br.com.dio.persistence;
+
+import br.com.dio.persistence.entity.EmployeeEntity;
+
+import java.util.List;
+
+public class EmployeeDAO {
+
+    public void insert(final EmployeeEntity entity){
+
+    }
+
+    public void update(final EmployeeEntity entity){
+
+    }
+
+    public void delete(final long id){
+
+    }
+
+    public List<EmployeeEntity> findAll(){
+        return null;
+    }
+
+    public EmployeeEntity findById(final long id){
+        return null;
+    }
+
+}
